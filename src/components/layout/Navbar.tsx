ac33fe684653f39543f34f9a8b8cdee5348eaeed
Atervaldo <hraujo@gmail.com>
2025-02-15 08:52:42 -0300

    Implementado paginação e viszualização de posts por autor

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -57,18 +57,18 @@ const navLinks = [
     path: "/posts",
     icon: <Article />,
   },
-  {
-    id: "6",
-    title: "Posts 3",
-    path: "/post3",
-    icon: <Article />,
-  },
-  {
-    id: "7",
-    title: "Posts 2",
-    path: "/post2",
-    icon: <Article />,
-  }
+  // {
+  //   id: "6",
+  //   title: "Posts 3",
+  //   path: "/post3",
+  //   icon: <Article />,
+  // },
+  // {
+  //   id: "7",
+  //   title: "Posts 2",
+  //   path: "/post2",
+  //   icon: <Article />,
+  // }
 ];
 
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
-import { DataTable } from "../pages/DataTable";
+import { UserList } from "../pages/users/UserList";
 import { Profile } from "../pages/Profile";
 import { PostList } from "../pages/posts/PostList";
-import { Post3 } from "../pages/posts/Post3";
-import { Posts } from "../pages/posts/Posts";
+
 
 
 export function MainRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/users" element={<DataTable entity="users" />} />
-            <Route path="/depts" element={<DataTable entity="departments" />} />
-            <Route path="/profile" element={ <Profile />} />
+            <Route path="/users" element={<UserList entity="users" />} />
+            <Route path="/depts" element={<UserList entity="departments" />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/posts" element={< PostList entity="posts" />} />
-            <Route path="/posts2" element={< Posts entity="posts" />} />
-
-            <Route path="/post3" element={<Post3 />} />
         </Routes>
     );
 }
